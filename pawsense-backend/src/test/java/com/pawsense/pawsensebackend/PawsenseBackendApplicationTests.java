00aafcd53e68ed2c14b20cbf94edf365837eaf65
karellehofler <karellehofler@gmail.com>
2023-05-25 23:15:54 -0400

    fixes to backend

---
+++ pawsense-backend/src/test/java/com/pawsense/pawsensebackend/PawsenseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawsense.pawsensebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsenseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
